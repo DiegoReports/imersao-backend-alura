89533791ff9a001509be20bce550c6a21e569bb0
Diego Henrique <diego.reports@gmail.com>
2024-11-20 15:11:23 -0300

    Aula 2: Criando e estruturando sua primeira API com GET e BD

--- server.js
+++ server.js
@@ -1,10 +1,82 @@
-import express from "express"
+import express from 'express';
+
+const posts = [
+  {
+    id: 1,
+    descricao: 'Uma foto teste',
+    imagem: 'https://placecats.com/millie/300/150'
+  },
+  {
+    id: 2,
+    descricao: 'Uma foto teste',
+    imagem: 'https://placecats.com/millie/300/150'
+  },
+  {
+    id: 3,
+    descricao: 'Gato preto e branco',
+    imagem: 'https://placecats.com/any/200/300'
+  },
+  {
+    id: 4,
+    descricao: 'Gatinho fofo dormindo',
+    imagem: 'https://placecats.com/cute/150/200'
+  },
+  {
+    id: 5,
+    descricao: 'Gato com olhos azuis',
+    imagem: 'https://placecats.com/blue/300/200'
+  },
+  {
+    id: 6,
+    descricao: 'Gato fazendo careta',
+    imagem: 'https://placecats.com/funny/250/250'
+  },
+  {
+    id: 7,
+    descricao: 'Gato de perfil',
+    imagem: 'https://placecats.com/profile/200/300'
+  },
+  {
+    id: 8,
+    descricao: 'Gato com chapéu',
+    imagem: 'https://placecats.com/hat/150/200'
+  },
+  {
+    id: 9,
+    descricao: 'Gato brincando com bola',
+    imagem: 'https://placecats.com/play/300/200'
+  },
+  {
+    id: 10,
+    descricao: 'Gato olhando para a câmera',
+    imagem: 'https://placecats.com/stare/250/250'
+  }
+];
 
 const app = express();
-app.listen(3000, ()=> {
-  console.log("Server is running on port 3000 ...");
+
+// Indicando funcionalidade de Parse em JSON
+app.use(express.json());
+
+// Iniciando o servidor [Port 3000]
+app.listen(3000, () => {
+  console.log('Server is running on port 3000 ...');
 });
 
-app.get("/api", (req, res) => {
-  res.status(200).send("Boas vindas a Imersão!")
-})
+// Definindo rota [all posts]
+app.get('/posts', (req, res) => {
+  res.status(200).json(posts)
+});
+
+// Função Buscar Post por ID
+function buscarPostPorId(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id)
+  });
+};
+
+// Definindo rota [single post]
+app.get('/posts/:id', (req, res) => {
+  const index = buscarPostPorId(req.params.id)
+  res.status(200).json(posts[index]);
+});
