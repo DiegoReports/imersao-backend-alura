3a6a91d6c60d421ed8b10b2f5f1f1edebe5e4454
Diego Henrique <diego.reports@gmail.com>
2024-11-23 13:02:03 -0300

    Aula 5 - Instalando dependencia Google Generative AI

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,6 @@
 import {getTodosPosts, criarPost, atualizarPost} from "../models/postsModel.js";
 import fs from "fs";
+import gerarDescricaoComGemini from "../services/geminiService.js";
 
 export async function listarPosts (req, res) {
   // Chama a função para obter os posts do banco de dados
@@ -47,13 +48,19 @@ export async function uploadImagem(req, res) {
 export async function atualizarNovoPost(req, res) {
   const id = req.params.id;
   const urlImagem = `http://localhost:3000/${id}.png`;
-  const post = {
-    imgUrl: urlImagem,
-    descricao: req.body.descricao,
-    alt: req.body.alt
-  }
+  
   try {
+    const imgBuffer =  fs.readFileSync(`uploads/${id}.png`);
+    const descricao = await gerarDescricaoComGemini(imgBuffer);
+
+    const post = {
+      imgUrl: urlImagem,
+      descricao: descricao,
+      alt: req.body.alt
+    }
+    
     const postCriado = await atualizarPost(id, post);
+
     res.status(200).json(postCriado);
   } catch (erro) {
     console.error(erro.message);
