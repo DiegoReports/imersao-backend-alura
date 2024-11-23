b78bde28a1c1fe77a797b48916174a4c22ccc451
Diego Henrique <diego.reports@gmail.com>
2024-11-22 21:09:13 -0300

    Aula - 4 Implementando armazenamento e upload de imagens

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -20,19 +20,26 @@ export async function postarNovoPost(req, res) {
 }
 
 export async function uploadImagem(req, res) {
+  // Cria um novo objeto para representar o post a ser inserido
   const novoPost = {
-      descricao: "",
-      imgUrl: req.file.originalname,
-      alt: ""
+      descricao: "", // Descrição do post (pode ser preenchida posteriormente)
+      imgUrl: req.file.originalname, // Nome original do arquivo enviado
+      alt: "" // Texto alternativo para a imagem (pode ser preenchido posteriormente)
   };
 
   try {
+    // Chama a função criarPost para inserir o novo post no banco de dados
       const postCriado = await criarPost(novoPost);
+    // Cria um novo nome para a imagem, utilizando o ID do post inserido
       const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+
+    // Move a imagem para a pasta de uploads com o novo nome
       fs.renameSync(req.file.path, imagemAtualizada)
+    // Retorna o post criado como resposta da requisição
       res.status(200).json(postCriado);  
   } catch(erro) {
       console.error(erro.message);
+    // Envia uma resposta com status 500 (erro interno do servidor)
       res.status(500).json({"Erro":"Falha na requisição"})
   }
 }
