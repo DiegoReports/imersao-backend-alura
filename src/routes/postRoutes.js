6a0fe1b4da2c256142859ffd7bb842539272fede
Diego Henrique <diego.reports@gmail.com>
2024-11-25 10:52:48 -0300

    Aula - 5 Integrando aplicacao ao front, cors e variavel cloud

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import multer from 'multer';
 import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from '../controllers/postsController.js';
+import cors from 'cors';
+
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200
+}
 
 // Config Multer p/Windows
 const storage = multer.diskStorage({
@@ -17,6 +23,8 @@ const upload = multer({dest: "./uploads" , storage});
 const routes = (app) => {
   // Indicando funcionalidade de Parse em JSON
   app.use(express.json());
+  // Lidando com requisições externas
+  app.use(cors(corsOptions));
   // Rota para obter todos os posts [all posts]
   app.get('/posts', listarPosts);
   // Rota para criar um novo post
